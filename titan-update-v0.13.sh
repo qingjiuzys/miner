cbd5e56331adb3672b099cf7bb7a5a59724819dc
qingjiu <142193034+qingjiuzys@users.noreply.github.com>
2024-03-23 03:43:00 +0800

    Update titan-update-v0.13.sh

--- titan-update-v0.13.sh
+++ titan-update-v0.13.sh
@@ -1,47 +1,65 @@
 #!/bin/bash
 
-# 检查输入参数
-if [ "$#" -ne 1 ]; then
-    echo "使用方法: $0 <新的存储大小GB>"
-    exit 1
-fi
+storage=2048 # 新增容器限制大小
+
+show_help() {
+    cat << EOF
+
+################################### 帮助信息 ###################################
+
+Usage: ${0##*/} [OPTIONS]
+
+OPTIONS:
+    --storage STORAGE        需要更新管理的存储空间大小。                          
+    -h / --help              显示此帮助信息并退出。
+
+资源链接:
+    - 微信：checkHeart666
+    - 代码库（欢迎点赞）： https://github.com/qingjiuzys/titan-start
+    - titan注册链接：https://test1.titannet.io/intiveRegister?code=wLFnFN
+    - meson注册链接：https://dashboard.gaganode.com/register?referral_code=qpkofealpfaomjb
+    - titan官网：https://titannet.io/
+    - titan存储服务：https://storage.titannet.io/
+    - titan测试节点控制台：https://test1.titannet.io/
+    - titan中文文档：https://titannet.gitbook.io/titan-network-cn
+
+################################################################################
+EOF
+}
+
+# 处理命令行参数
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        --storage=*) storage="${1#*=}" ;;
+        -h|--help) show_help; exit 0 ;;
+        *) echo "未知参数: $1" ; show_help; exit 1 ;;
+    esac
+    shift
+done
+
 
 # 定义变量
-new_storage_size=$1 # 从命令行参数获取新的存储大小，单位GB
-storage_dirs_prefix="/mnt/titan/storage-" # 存储目录前缀
+new_storage_size=$storage # 从命令行参数获取新的存储大小，单位GB
+storage_dirs_prefix="/mnt/storage-" # 存储目录前缀
 target_data_dir="/mnt/data" # 目标数据目录
 containers=5 # 容器数量
 image_name="nezha123/titan-edge:1.0" # Docker镜像名称
+new_image_name="nezha123/titan-edge:1.2" # Docker镜像名称
 
-# 检查并创建目标数据目录
-if [ ! -d "$target_data_dir" ]; then
-    echo "创建目标数据目录：$target_data_dir"
-    mkdir -p "$target_data_dir"
-fi
 
 # 移动storage目录下的assets文件，并修改config.toml配置
 for i in $(seq 1 $containers); do
-    storage_dir="${storage_dirs_prefix}${i}"
-    config_file="$storage_dir/config.toml"
-
-    echo "处理存储目录：$storage_dir"
-
-    # 移动文件
-    if [ -d "$storage_dir/storage/assets" ]; then
-        echo "移动 $storage_dir/storage/assets 到 $target_data_dir"
-        mv "$storage_dir/storage/assets"/* "$target_data_dir/"
-    else
-        echo "目录 $storage_dir/storage/assets 不存在，跳过"
-    fi
-
+    config_file="$storage_dirs_prefix$i/config.toml"
     # 修改config.toml中的存储大小
     if [ -f "$config_file" ]; then
         echo "修改 $config_file 中的存储大小为 $new_storage_size GB"
-        sed -i "s/#StorageGB = [0-9]*/StorageGB = $new_storage_size/" "$config_file"
+        sed -i "s/#StorageGB = [0-9]*/StorageGB = $storage/" "$config_file"
+        sed -i "s/StorageGB = [0-9]*/StorageGB = $storage/" "$config_file"
     else
         echo "配置文件 $config_file 不存在，跳过"
     fi
 done
+
 
 # 停止并删除相关容器
 for i in $(seq 1 $containers); do
@@ -54,12 +72,13 @@ done
 # 删除Docker镜像
 echo "删除Docker镜像：$image_name"
 docker rmi "$image_name"
+docker pull "$new_image_name"
 
 # 重新启动容器
 for i in $(seq 1 $containers); do
     container_name="titan-edge0$i"
-    echo "重新启动容器：$container_name"
-    docker run --name "$container_name" -d -v "${storage_dirs_prefix}${i}:/root/.titanedge" -v "$target_data_dir:/root/.titanedge/storage/assets" "$image_name"
+    echo "重新启动容器：$new_image_name"
+    docker run --name "$container_name" -d -v "${storage_dirs_prefix}${i}:/root/.titanedge"  "$new_image_name"
 done
 
-echo "迁移和容器更新完成。"
+echo "迁移和容器重启完成。"
