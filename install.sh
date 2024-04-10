f58b0ef927a0f9adb1dc629172743639a3e2e8d3
qingjiu <142193034+qingjiuzys@users.noreply.github.com>
2024-04-10 15:37:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -179,8 +179,8 @@ set_storage(){
     for i in $(seq 1 $containers)
     do
         echo "******************正在修改容器$titan-edge0$i的存储配置******************"
-        sed -i "s/#StorageGB = [0-9]*/StorageGB = $storage/" "$config_file"
-        sed -i "s/StorageGB = [0-9]*/StorageGB = $storage/" "$config_file"
+        sed -i "s/#StorageGB = [0-9]*/StorageGB = $storage/" "/mnt/storage-$i"
+        sed -i "s/StorageGB = [0-9]*/StorageGB = $storage/" "/mnt/storage-$i"
         echo "******************重启容器$titan-edge0$i以应用新的存储配置******************"
         docker restart titan-edge0$i
     done
